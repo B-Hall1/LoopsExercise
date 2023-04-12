b90a3b0b530bf1b769be42c8fd47b05edd36571e
Blair Hall <blair.hall.og@gmail.com>
2023-04-11 21:08:03 -0400

    Created

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let i = 1; i < 8; i++) {
+    console.log(i);
+}
+
+for(let i = 5; i < 29; i = i+4) {
+    console.log(i);
+}
+
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"]
+
+for (let x of wizards) {
+    console.log(x);
+}
+
+let harryPotterMovies = 0;
+while (harryPotterMovies != 8) {
+    harryPotterMovies++;
+}
+console.log(harryPotterMovies);
